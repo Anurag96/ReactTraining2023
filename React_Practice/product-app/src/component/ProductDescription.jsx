8fa86fc0e2fc81af404dd1408c0fd31e528322f2
Anurag Kumar <c-anurag.kumar@charter.com>
2023-12-18 15:35:13 -0600

    Added product description

--- React_Practice/product-app/src/component/ProductDescription.jsx
+++ React_Practice/product-app/src/component/ProductDescription.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import './ProductDescription.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProductDescription() {
     const [data, setData] = useState([]);
     const fetchData = async () => {
         // Actual API Call
         try {
-            const baseURL = 'https://fakestoreapi.com/products/1';
+            const baseURL = 'https://fakestoreapi.com/products/12';
             const response = await axios.get(`${baseURL}`)
                 .then(response => response.data)
                 .then(response => {
@@ -34,11 +35,11 @@ function ProductDescription() {
         margin: 5,
         paddingLeft: 0,
         minWidth: 240,
-      };
+    };
 
     return (
         <div className='wrapper'>
-            {JSON.stringify(data)}
+            {/* {JSON.stringify(data)} */}
             <br />
 
             {(<>
@@ -71,6 +72,50 @@ function ProductDescription() {
                     </div>
                 </> */}
             </>)}
+
+
+            <div>
+                <div class="container bootdey">
+                    <div class="row">
+                        <section class="panel">
+                            <div class="panel-body">
+                                <div class="row row-no-gutters">
+                                    <div class="col-sm-4 mx-4" style={{ backgroundColor: "lavender" }}>
+                                        <div class="pro-img-details">
+                                            <img src={data.image} alt="" />
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-7" style={{ backgroundColor: "lavenderblush" }}>
+                                        <h4 class="pro-d-title">
+                                            <a href="#/" class="">
+                                                {data.title}
+                                            </a>
+                                        </h4>
+                                        <p>
+                                            {data.description}
+                                        </p>
+                                        <div class="product_meta">
+                                            <span class="posted_in"> <strong>Categories:</strong> {data.category}</span>
+                                            <span class="posted_in"> <strong>Rating:</strong> {data.rating.rate}</span>
+
+                                        </div>
+                                        <div class="m-bot15"> <strong>Price : </strong> <span class="pro-price"> ${data.price}</span></div>
+                                        <div class="form-group">
+                                            <label>Quantity</label>
+                                            <input type="quantiy" placeholder="1" class="form-control quantity" />
+                                        </div>
+                                        <p>
+                                            <button class="btn btn-round btn-danger" type="button"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
