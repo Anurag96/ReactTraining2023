61b47d0bea96c61a3d8970ad43397b4f623bfc55
Anurag Kumar <anuragkumarrr@gmail.com>
2023-12-18 19:20:33 -0600

    updated the css

--- React_Practice/product-app/src/component/ProductDescription.jsx
+++ React_Practice/product-app/src/component/ProductDescription.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import './ProductDescription.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate, useParams } from "react-router-dom";
 
 function ProductDescription() {
