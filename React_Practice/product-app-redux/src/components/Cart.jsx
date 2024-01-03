9519ff911cb671e6b08ed0e914a79f83f47ed553
Anurag Kumar <c-anurag.kumar@charter.com>
2024-01-03 11:17:22 -0600

    updated the cart

--- React_Practice/product-app-redux/src/components/Cart.jsx
+++ React_Practice/product-app-redux/src/components/Cart.jsx
@@ -3,24 +3,38 @@ import { useSelector } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardFooter from 'react-bootstrap/esm/CardFooter';
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBCardText,
+    MDBCol,
+    MDBContainer,
+    MDBIcon,
+    MDBInput,
+    MDBRow,
+    MDBTypography,
+} from "mdb-react-ui-kit";
+
 
 function Cart() {
     const productCart = useSelector(state => state.cart)
     const productList = (productCart).map((e) => (
         <div className='col-md-3' style={{ marginBottom: '2rem' }}>
             <Card className='h-100' >
-                <div className='text-center'><Card.Img variant="top" src={e.image} style={{ width: '180px', height: '180px' }} /></div>
+                <div className='text-center'><Card.Img variant="top" src={e.image} style={{ width: '80px', height: '80px' }} /></div>
                 <Card.Body>
                     <Card.Title>{e.title}</Card.Title>
                     <Card.Text>
-                        Price : ${e.price} || Rating : {e?.rating?.rate}
+                        Price : ${e.price}
                     </Card.Text>
                     <Card.Text>
                         Category : {e.category.toUpperCase()}
                     </Card.Text>
-                    <Card.Text>
+                    {/* <Card.Text>
                         {e.description}
-                    </Card.Text>
+                    </Card.Text> */}
                 </Card.Body>
                 {/* <CardFooter>
                     <Button variant="primary" onClick={() => addToCart(e)}>Add to Cart</Button>
@@ -30,10 +44,122 @@ function Cart() {
     ))
     return (
         <div>
-            <h1>Carts</h1>
+            <h1>Shopping Cart</h1>
             <div className="row">
-                {productList}
+                {/* {productList} */}
             </div>
+
+            <section className="h-100 h-custom" style={{ backgroundColor: "#eee" }}>
+                <MDBContainer >
+                    <MDBRow className="justify-content-center align-items-center h-100">
+                        <MDBCol size="12">
+                            <MDBCard className="card-registration card-registration-2" style={{ borderRadius: "15px" }}>
+                                <MDBCardBody className="p-0">
+                                    <MDBRow className="g-0">
+                                        <MDBCol lg="8">
+                                            <div className="p-5">
+                                                <div className="d-flex justify-content-between align-items-center mb-5">
+                                                    {/* <MDBTypography tag="h1" className="fw-bold mb-0 text-black">
+                                                        Shopping Cart
+                                                    </MDBTypography> */}
+                                                    <MDBTypography className="mb-0 text-muted">
+                                                        Total : {productCart.length} Item/s
+                                                    </MDBTypography>
+                                                </div>
+                                                {
+                                                    (productCart).map((e) => (
+                                                        <>
+                                                            <MDBRow className="mb-4 d-flex justify-content-between align-items-center">
+                                                                <MDBCol md="2" lg="2" xl="2">
+                                                                    <MDBCardImage
+                                                                        src={e.image}
+                                                                        fluid className="rounded-3" alt="Cotton T-shirt" />
+                                                                </MDBCol>
+                                                                <MDBCol md="3" lg="3" xl="3">
+                                                                    <MDBTypography tag="h6" className="text-muted">
+                                                                        {e.category}
+                                                                    </MDBTypography>
+                                                                    <MDBTypography tag="h6" className="text-black mb-0">
+                                                                        {e.title}
+                                                                    </MDBTypography>
+                                                                </MDBCol>
+                                                                <MDBCol md="3" lg="3" xl="3" className="d-flex align-items-center">
+                                                                    <MDBBtn color="link" className="px-2">
+                                                                        <MDBIcon fas icon="minus" />
+                                                                    </MDBBtn>
+
+                                                                    <MDBInput type="number" min="0" defaultValue={1} size="sm" />
+
+                                                                    <MDBBtn color="link" className="px-2">
+                                                                        <MDBIcon fas icon="plus" />
+                                                                    </MDBBtn>
+                                                                </MDBCol>
+                                                                <MDBCol md="3" lg="2" xl="2" className="text-end">
+                                                                    <MDBTypography tag="h6" className="mb-0">
+                                                                        {e.price}
+                                                                    </MDBTypography>
+                                                                </MDBCol>
+                                                                <MDBCol md="1" lg="1" xl="1" className="text-end">
+                                                                    <a href="#!" className="text-muted">
+                                                                        <MDBIcon fas icon="times" />
+                                                                    </a>
+                                                                </MDBCol>
+                                                            </MDBRow>
+                                                            <hr className="my-4" />
+                                                        </>
+                                                    ))
+                                                }
+
+
+                                                {/* <MDBRow className="mb-4 d-flex justify-content-between align-items-center">
+                                                            <MDBCol md="2" lg="2" xl="2">
+                                                                <MDBCardImage
+                                                                    src={e.image}
+                                                                    fluid className="rounded-3" alt="Cotton T-shirt" />
+                                                            </MDBCol>
+                                                            <MDBCol md="3" lg="3" xl="3">
+                                                                <MDBTypography tag="h6" className="text-muted">
+                                                                    {e.category}
+                                                                </MDBTypography>
+                                                                <MDBTypography tag="h6" className="text-black mb-0">
+                                                                    {e.title}
+                                                                </MDBTypography>
+                                                            </MDBCol>
+                                                            <MDBCol md="3" lg="3" xl="3" className="d-flex align-items-center">
+                                                                <MDBBtn color="link" className="px-2">
+                                                                    <MDBIcon fas icon="minus" />
+                                                                </MDBBtn>
+
+                                                                <MDBInput type="number" min="0" defaultValue={1} size="sm" />
+
+                                                                <MDBBtn color="link" className="px-2">
+                                                                    <MDBIcon fas icon="plus" />
+                                                                </MDBBtn>
+                                                            </MDBCol>
+                                                            <MDBCol md="3" lg="2" xl="2" className="text-end">
+                                                                <MDBTypography tag="h6" className="mb-0">
+                                                                    {e.price}
+                                                                </MDBTypography>
+                                                            </MDBCol>
+                                                            <MDBCol md="1" lg="1" xl="1" className="text-end">
+                                                                <a href="#!" className="text-muted">
+                                                                    <MDBIcon fas icon="times" />
+                                                                </a>
+                                                            </MDBCol>
+                                                        </MDBRow> */}
+
+
+                                            </div>
+                                        </MDBCol>
+                                    </MDBRow>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </section>
+
+
         </div>
     )
 }
